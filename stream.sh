21a72135817e232a8d210e03a8c7b4c5a08fb810
Karthikeyan <75429419+Karthikeyandevp@users.noreply.github.com>
2026-02-26 18:25:40 +0530

    Change input file for ffmpeg streaming

--- stream.sh
+++ stream.sh
@@ -1,4 +1,4 @@
-ffmpeg -re -stream_loop -1 -i live.mp4 \
+ffmpeg -re -stream_loop -1 -i 1000084112.mp4 \
 vf "scale=1080:1920" \
 -c:v libx264 -preset veryfast -maxrate 3000k -bufsize 6000k \
 -pix_fmt yuv420p -g 50 \
