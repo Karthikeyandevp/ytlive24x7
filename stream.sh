e33d1f4ce0a0c9325d64d12d1162d23694ec30b0
Karthikeyan <75429419+Karthikeyandevp@users.noreply.github.com>
2026-02-26 19:12:55 +0530

    Fix duplicate scale filter in stream.sh
    Removed duplicate video filter option in ffmpeg command.

--- stream.sh
+++ stream.sh
@@ -1,5 +1,5 @@
 ffmpeg -re -stream_loop -1 -i live.mp4 \
-vf "scale=1080:1920" \
+-vf "scale=1080:1920" \
 -c:v libx264 -preset veryfast -maxrate 3000k -bufsize 6000k \
 -pix_fmt yuv420p -g 50 \
 -c:a aac -b:a 128k -ar 44100 \
