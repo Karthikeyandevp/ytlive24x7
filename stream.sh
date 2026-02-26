daba26c68edbf2a7c53bba983fb7266dc9ac1301
Karthikeyan <75429419+Karthikeyandevp@users.noreply.github.com>
2026-02-26 17:29:50 +0530

    Delete stream.sh

--- stream.sh
+++
@@ -1,4 +0,0 @@
-ffmpeg -re -stream_loop -1 -i live.mp4 \
--c:v libx264 -preset veryfast -maxrate 3000k -bufsize 6000k \
--pix_fmt yuv420p -g 50 -c:a aac -b:a 128k \
--f flv rtmp://a.rtmp.youtube.com/live2/wghq-2ghu-dqyj-qaee-5zs4
